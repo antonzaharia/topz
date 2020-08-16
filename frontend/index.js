f540f419244c5a94bdf7d008a8c0bd6c38518fbf
antonzaharia <zaharia_anton@yahoo.com>
2020-08-16 18:10:23 +0100

    option class added

--- frontend/index.js
+++ frontend/index.js
@@ -9,8 +9,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
         return response.json();
     })
     .then(function(tops){
-        // console.log(tops);
-        // loadTops(tops);
         allTops = Top.createTops(tops)
         Top.loadTops(allTops);
     })
@@ -36,26 +34,42 @@ class Top {
             let title = document.createElement("h3");
             
             title.textContent = top.title;
+            div.className = "list-group"
             div.appendChild(title);
             main.appendChild(div);
     
-            top.loadOptions(div)
+            Option.loadOptions(top.options, div);
         }
     }
 
-    loadOptions(div){
-    let ul = document.createElement("ul")
-    for (let option of this.options){
-        let li = document.createElement("li")
-        let votes = document.createElement("span")
-
-        li.textContent = option["content"]
-        votes.textContent = option["votes"]
-        li.appendChild(votes)
-        ul.appendChild(li)
-
-    }
-    div.appendChild(ul);
-  }
 }
+
+class Option {
+    constructor(content, votes) {
+        this.content = content;
+        this.votes = votes;
+    }
+    static loadOptions(topOptions, whereTo) {
+        let div = document.createElement("div")
+        for (let option of topOptions){
+            let a = document.createElement("a")
+    
+            a.href = "#"
+            a.textContent = option["content"]
+            a.className = "list-group-item list-group-item-action list-group-item-secondary"
+    
+            div.appendChild(a)
+    
+        }
+        whereTo.appendChild(div);
+    }
+}
+
+// class createHTML {
+//     static button(appendTo, content){
+//         let button = document.createElement('button');
+//         button.textContent = content;
+//         appendTo.appendChild(button);
+//     }
+// }
 
