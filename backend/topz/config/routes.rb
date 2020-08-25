1e7da90525ad724d34589322f15b8a6441d470d0
antonzaharia <zaharia_anton@yahoo.com>
2020-08-25 16:57:10 +0100

    removed the routes did not used

--- backend/topz/config/routes.rb
+++ backend/topz/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :options
-  resources :tops
+  resources :options, only: [:create, :update, :destroy]
+  resources :tops, only: [:index, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
